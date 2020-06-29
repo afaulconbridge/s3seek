cbde837501dcce6c84a0ce22c06a812646f7e371
Adam Faulconbridge <afaulconbridge@googlemail.com>
2020-06-29 22:28:37 +0100

    initial

---
+++ s3seek/__init__.py
@@ -0,0 +1,188 @@
+import io
+
+
+class S3File(io.RawIOBase):
+    def __init__(self, s3_object):
+        self.s3_object = s3_object
+        self.position = 0
+
+    def __repr__(self):
+        return f"S3File({self.s3_object})"
+
+    @property
+    def size(self):
+        # TODO check if this is cached or always requested
+        return self.s3_object.content_length
+
+    def seekable(self):
+        return True
+
+    def tell(self):
+        return self.position
+
+    def seek(self, offset, whence=io.SEEK_SET):
+        if whence == io.SEEK_SET:
+            if offset < 0:
+                raise ValueError("Unable to seek before start")
+            if offset > self.size:
+                raise ValueError("Unable to seek after end")
+            self.position = offset
+        elif whence == io.SEEK_CUR:
+            if self.position + offset < 0:
+                raise ValueError("Unable to seek before start")
+            if self.position + offset > self.size:
+                raise ValueError("Unable to seek after end")
+            self.position += offset
+        elif whence == io.SEEK_END:
+            if offset < -self.size:
+                raise ValueError("Unable to seek before start")
+            if offset > 0:
+                raise ValueError("Unable to seek after end")
+            self.position = self.size + offset
+        else:
+            raise ValueError(
+                "invalid whence (%r, should be %d, %d, %d)"
+                % (whence, io.SEEK_SET, io.SEEK_CUR, io.SEEK_END)
+            )
+
+        return self.position
+
+    def readable(self):
+        return True
+
+    def read(self, size=-1):
+        if size == -1 and self.position == 0:
+            # special case, skip range header if reading all
+            return self.s3_object.get()["Body"].read()
+        elif size == -1:
+            # Read to the end of the file
+            range_header = f"bytes={self.position}-{self.size}"
+            self.seek(offset=0, whence=io.SEEK_END)
+        else:
+            new_position = self.position + size
+
+            # If we're going to read beyond the end of the object, return
+            # the entire object.
+            if new_position >= self.size:
+                return self.read()
+
+            range_header = f"bytes={self.position}-{new_position - 1}"
+            self.seek(offset=size, whence=io.SEEK_CUR)
+
+        return self.s3_object.get(Range=range_header)["Body"].read()
+
+    def writable(self):
+        return False
+
+    def truncate(self):
+        raise OSError("not writable")
+
+    def write(self, bytes_to_write):
+        raise OSError("not writable")
+
+
+class S3FileBuffered(io.BufferedIOBase):
+    def __init__(self, s3_object, buffer_max=1048576):
+        self.s3_object = s3_object
+        self.position = 0
+        self.buffer_max = buffer_max  # default 1MB = 1048576 bytes
+        self.buffer = b""
+        self.raw = S3File(s3_object)
+
+    def __repr__(self):
+        return f"S3FileBuffered({self.s3_object})"
+
+    @property
+    def size(self):
+        return self.raw.size
+
+    def seekable(self):
+        return True
+
+    def tell(self):
+        return self.position
+
+    def seek(self, offset, whence=io.SEEK_SET):
+        old_position = self.position
+
+        if whence == io.SEEK_SET:
+            if offset < 0:
+                raise ValueError("Unable to seek before start")
+            if offset > self.size:
+                raise ValueError("Unable to seek after end")
+            self.position = offset
+        elif whence == io.SEEK_CUR:
+            if self.position + offset < 0:
+                raise ValueError("Unable to seek before start")
+            if self.position + offset > self.size:
+                raise ValueError("Unable to seek after end")
+            self.position += offset
+        elif whence == io.SEEK_END:
+            if offset < -self.size:
+                raise ValueError("Unable to seek before start")
+            if offset > 0:
+                raise ValueError("Unable to seek after end")
+            self.position = self.size + offset
+        else:
+            raise ValueError(
+                "invalid whence (%r, should be %d, %d, %d)"
+                % (whence, io.SEEK_SET, io.SEEK_CUR, io.SEEK_END)
+            )
+
+        # need to update the buffer as we moved
+        # if its a small jump forward, jump the buffer
+        if self.position > old_position and self.position < old_position + len(
+            self.buffer
+        ):
+            jump = self.position - old_position
+            print(f"Trimming buffer by {jump}")
+            print(self.buffer)
+            print(self.buffer[jump:])
+            self.buffer = self.buffer[jump:]
+        else:
+            # too big a change, empty the buffer
+            print("Empting buffer")
+            self.buffer = b""
+
+        return self.position
+
+    def readable(self):
+        return True
+
+    def read(self, size=-1):
+        if size >= 0:
+            return self.read1(size)
+        else:
+            return self.read1(self.size - self.position)
+
+    def read1(self, size):
+        if size <= len(self.buffer):
+            # request fits in current buffer
+            # no need to get more data
+            value = self.buffer[:size]
+            self.buffer = self.buffer[size:]
+            self.seek(size, io.SEEK_CUR)
+            return value
+        else:
+            # need to grow buffer
+            # calculate how much more we need
+            size_to_get = size - len(self.buffer) + self.buffer_max
+            # move to the appropriate place in the raw stream
+            self.raw.seek(self.position + len(self.buffer), io.SEEK_SET)
+            # get the extra data
+            self.buffer = self.buffer + self.raw.read(size_to_get)
+            # get the piece to return
+            value = self.buffer[:size]
+            # move the position and trim the buffer
+            self.seek(size, io.SEEK_CUR)
+            # return the value we put together earlier
+            return value
+
+    def writable(self):
+        return False
+
+    def truncate(self):
+        raise OSError("not writable")
+
+    def write(self, bytes_to_write):
+        raise OSError("not writable")
---
+++ tests/s3seek_test.py
@@ -0,0 +1,154 @@
+import io
+
+import pytest
+
+from s3seek import S3File, S3FileBuffered
+
+
+@pytest.fixture
+def s3_obj():
+    # this is a public file we can use
+    # not ideal, but simpler than mocking for now
+    import boto3
+
+    resource = boto3.resource("s3")
+    from botocore.handlers import disable_signing
+
+    resource.meta.client.meta.events.register("choose-signer.s3.*", disable_signing)
+    obj = resource.Object(bucket_name="1000genomes", key="CHANGELOG")
+    return obj
+
+
+class TestS3File:
+    def test_info(self, s3_obj):
+        s3file = S3File(s3_obj)
+        assert s3file.seekable()
+        assert s3file.readable()
+
+    def test_not_writable(self, s3_obj):
+        s3file = S3File(s3_obj)
+        assert not s3file.writable()
+        with pytest.raises(OSError):
+            assert s3file.write(b"")
+        with pytest.raises(OSError):
+            assert s3file.truncate()
+
+    def test_read_all(self, s3_obj):
+        content = s3_obj.get()["Body"].read()
+        s3file = S3File(s3_obj)
+
+        # read the whole thing
+        assert content == s3file.read()
+        assert len(content) == s3file.size
+
+    def test_seek(self, s3_obj):
+        content = s3_obj.get()["Body"].read()
+        s3file = S3File(s3_obj)
+
+        # read the first X bytes
+        content = s3_obj.get()["Body"].read()
+        assert content[:16] == s3file.read(16)
+        assert 16 == s3file.tell()
+
+        # seek back to the start
+        s3file.seek(0)
+        assert content[:16] == s3file.read(16)
+        s3file.seek(0, io.SEEK_SET)
+        assert content[:16] == s3file.read(16)
+
+        # relative seek forward
+        s3file.seek(16, io.SEEK_CUR)
+        assert content[32:48] == s3file.read(16)
+
+        # relative seek backward
+        s3file.seek(-16, io.SEEK_CUR)
+        assert content[32:48] == s3file.read(16)
+        # relative seek end
+        s3file.seek(-16, io.SEEK_END)
+        assert content[-16:] == s3file.read(16)
+
+        # seek outside of file
+        with pytest.raises(ValueError):
+            s3file.seek(-len(content) - 1, io.SEEK_CUR)
+        with pytest.raises(ValueError):
+            s3file.seek(len(content), io.SEEK_CUR)
+        with pytest.raises(ValueError):
+            s3file.seek(-1, io.SEEK_SET)
+        with pytest.raises(ValueError):
+            s3file.seek(len(content) + 1, io.SEEK_SET)
+        with pytest.raises(ValueError):
+            s3file.seek(-1)
+        with pytest.raises(ValueError):
+            s3file.seek(len(content) + 1)
+
+
+class TestS3FileBuffered:
+    def test_info(self, s3_obj):
+        s3file = S3FileBuffered(s3_obj, 64)
+        assert s3file.seekable()
+        assert s3file.readable()
+
+    def test_not_writable(self, s3_obj):
+        s3file = S3FileBuffered(s3_obj, 64)
+        assert not s3file.writable()
+        with pytest.raises(OSError):
+            assert s3file.write(b"")
+        with pytest.raises(OSError):
+            assert s3file.truncate()
+
+    def test_read_all(self, s3_obj):
+        content = s3_obj.get()["Body"].read()
+        s3file = S3FileBuffered(s3_obj, 64)
+
+        # read the whole thing
+        assert content == s3file.read()
+        assert len(content) == s3file.size
+
+    def test_seek(self, s3_obj):
+        content = s3_obj.get()["Body"].read()
+        s3file = S3FileBuffered(s3_obj, 64)
+
+        # first 80 bytes are:
+        # b'2015-09-04\n\nModification to: misc,bas\n\nDetails can be found in\nchangelog_details'
+        #   0                 16              32                 48                64
+
+        # read the first X bytes
+        content = s3_obj.get()["Body"].read()
+        assert content[:16] == s3file.read(16)
+        assert 16 == s3file.tell()
+
+        # seek back to the start
+        s3file.seek(0)
+        assert content[:16] == s3file.read(16)
+        s3file.seek(0, io.SEEK_SET)
+        assert content[:16] == s3file.read(16)
+
+        # relative seek forward
+        s3file.seek(16, io.SEEK_CUR)
+        assert content[32:48] == s3file.read(16)
+
+        # relative seek backward
+        s3file.seek(-16, io.SEEK_CUR)
+        assert content[32:48] == s3file.read(16)
+        # relative seek end
+        s3file.seek(-16, io.SEEK_END)
+        assert content[-16:] == s3file.read(16)
+
+        # read over buffer end
+        s3file.seek(0)
+        assert content[:16] == s3file.read(16)
+        assert content[16 : 16 + 128] == s3file.read(128)
+
+        # seek outside of file
+        with pytest.raises(ValueError):
+            s3file.seek(-len(content) - 1, io.SEEK_CUR)
+        with pytest.raises(ValueError):
+            s3file.seek(len(content), io.SEEK_CUR)
+        with pytest.raises(ValueError):
+            s3file.seek(-1, io.SEEK_SET)
+        with pytest.raises(ValueError):
+            s3file.seek(len(content) + 1, io.SEEK_SET)
+        with pytest.raises(ValueError):
+            s3file.seek(-1)
+        with pytest.raises(ValueError):
+            s3file.seek(len(content) + 1)
